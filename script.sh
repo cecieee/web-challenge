288f1036e64f8a7ab85f2250af3006b0bd020615
R-Aravind <aravind.ar786@gmail.com>
2020-03-28 02:33:39 +0530

    updated README

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@
 
 ls task-1 > names.txt 
 
-echo "TASK 1 LINKS" > README.MD
+echo "# TASK 1 LINKS" > README.MD
 
 awk '
 
@@ -15,8 +15,8 @@ BEGIN {
 }
 
 { 
-	print($0, " -- .github.io/task-1/", $0, "/", OFS="") 
-
+	print("* ", $0, ": [click here to view page] (https://cecieee.github.io/web-challenge/task-1/", $0, "/)", OFS="") 
+	
 }' names.txt >> README.MD
 
 rm names.txt
